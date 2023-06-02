ebfad28770e0f7bbec0bb803b3cbc1e4f0fd5da4
Gahee Choi(Kate) <gaheekate@gmail.com>
2023-06-02 17:57:58 +0900

    question.js

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,25 +1,69 @@
 import { useState, useEffect } from "react";
 
 export default function QuestionData() {
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState([]); // State variable to store the questions
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0); // State variable to track the current question index
+  const [currentAnswerWeight, setCurrentAnswerWeight] = useState(0); // State variable to track the current answer weight
+  const [selectedAnswer, setSelectedAnswer] = useState(0); // State variable to store the selected answer value
+  const [showResultPage, setShowResultPage] = useState(false); // State variable to control rendering of the result page
 
   useEffect(() => {
-    const fetchQuestions = async () => {
+    const fetchData = async () => {
       try {
-  
-        const baseUrl = process.env.REACT_APP_BASEURL;
-        const response = await fetch(`${baseUrl}/questions`);
-       
+        // Fetch questions data
+        const questionResponse = await fetch(`${process.env.REACT_APP_BASEURL}/questions`);
+        const questionData = await questionResponse.json();
 
-        const data = await response.json();
-        setQuestions(data);
+        // Fetch answers data
+        const answerResponse = await fetch(`${process.env.REACT_APP_BASEURL}/answers`);
+        const answerData = await answerResponse.json();
+
+        // Associate each answer with its corresponding question
+        const updatedQuestions = questionData.map((question) => {
+          const associatedAnswers = answerData.filter((answer) => answer.qu === question.num);
+          return { ...question, answers: associatedAnswers };
+        });
+
+        setQuestions(updatedQuestions); // Update the questions state with the updated data
       } catch (error) {
-        console.error("Error fetching questions:", error);
+        console.error("Error fetching data:", error);
       }
     };
 
-    fetchQuestions();
+    fetchData(); // Fetch data when the component mounts
   }, []);
+
+  const handleAnswerSelect = (questionId, answerValue) => {
+    setSelectedAnswer(answerValue); // Store the selected answer value
+    console.log("Selected answer:", questionId, answerValue);
+  };
+
+  const handleNextQuestion = () => {
+    setCurrentQuestionIndex((prevIndex) => prevIndex + 1); // Increment the current question index
+    setCurrentAnswerWeight((prevWeight) => prevWeight + selectedAnswer); // Accumulate the answer weight
+    setSelectedAnswer(""); // Reset the selected answer
+
+    if (currentQuestionIndex === questions.length - 1) {
+      setShowResultPage(true); // Show the result page when reaching the last question
+    }
+  };
+
+  if (questions.length === 0) {
+    return <div>Loading...</div>; // Display loading message while fetching data
+  }
+
+  const currentQuestion = questions[currentQuestionIndex]; // Get the current question based on the current index
+
+  if (showResultPage) {
+    return (
+      <div>
+        <h2>Result Page</h2>
+        <p>Total Answer Weight: {currentAnswerWeight}</p>
+      </div>
+    );
+  }
+
+  const isNextButtonDisabled = selectedAnswer === ""; // Check if an answer is selected
 
   return (
     <div>
@@ -27,11 +71,30 @@ export default function QuestionData() {
         <i className="whmcs icon"></i>
         <div className="content">Question List</div>
       </h2>
-      <ul style={{ marginBottom: "50px" }}>
-        {questions.map((question) => (
-          <li key={question._id}>{question.qu}</li>
-        ))}
-      </ul>
+      <div key={currentQuestion._id}>
+        <h3>{currentQuestion.qu}</h3>
+        <ul>
+          {currentQuestion.answers.map((answer) => (
+            <li key={answer.qu}>
+              <label>
+                <input
+                  type="radio"
+                  name={currentQuestion.num}
+                  value={answer.weight}
+                  checked={selectedAnswer === answer.weight} // Check if the answer is selected
+                  onChange={() => handleAnswerSelect(currentQuestion.num, answer.weight)}
+                />
+                {answer.answer}
+              </label>
+            </li>
+          ))}
+        </ul>
+      </div>
+      {currentQuestionIndex < questions.length && (
+        <button onClick={handleNextQuestion} disabled={isNextButtonDisabled}>
+          Next
+        </button> // Display "Next" button if there are more questions
+      )}
     </div>
   );
 }
